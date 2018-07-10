33ff24663e87af3749b2af0b538bd17afc9782a7
Jintong Wu <akudet@qq.com>
2018-07-10 16:00:20 +0800

    impl sample func

--- model.py
+++ model.py
@@ -14,11 +14,15 @@ class EncoderCNN(nn.Module):
         modules = list(resnet.children())[:-1]
         self.resnet = nn.Sequential(*modules)
         self.embed = nn.Linear(resnet.fc.in_features, embed_size)
+        self.bn = nn.BatchNorm1d(embed_size)
+
+        torch.nn.init.xavier_uniform_(self.embed.weight)
 
     def forward(self, images):
         features = self.resnet(images)
         features = features.view(features.size(0), -1)
         features = self.embed(features)
+        features = self.bn(features)
         return features
 
 
@@ -32,6 +36,8 @@ class DecoderRNN(nn.Module):
         self.embed = nn.Embedding(vocab_size, embed_size)
         self.lstm = nn.LSTM(embed_size, hidden_size, num_layers, batch_first=True)
         self.fc = nn.Linear(hidden_size, vocab_size)
+
+        torch.nn.init.xavier_uniform_(self.fc.weight)
 
     def init_hidden(self, batch_size):
         return (torch.zeros((self.num_layers, batch_size, self.hidden_size)),
@@ -53,4 +59,16 @@ class DecoderRNN(nn.Module):
 
     def sample(self, inputs, states=None, max_len=20):
         " accepts pre-processed image tensor (inputs) and returns predicted sentence (list of tensor ids of length max_len) "
-        pass
+        results = []
+        if not states:
+            states = self.init_hidden(inputs.shape[0])
+            states = states[0].to(inputs.device), states[1].to(inputs.device)
+        for i in range(max_len):
+            # [N, 1, H]
+            outputs, states = self.lstm(inputs, states)
+            # [N, V]
+            outputs = self.fc(outputs.squeeze(1))
+            _, outputs = outputs.max(1)
+            results.append(outputs.item())
+            inputs = self.embed(outputs.unsqueeze(1))
+        return results
